d68c164144305d09f1f91673df5315e41f712461
BehruzRehimli <121186387+BehruzRehimli@users.noreply.github.com>
2023-01-10 21:36:11 +0400

    Add files via upload

---
+++ əvvələ və sona 3 yazdır/test array/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace test_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine(a*10+30003);
+
+        }
+    }
+}
